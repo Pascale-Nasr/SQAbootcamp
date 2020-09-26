255a846aef323703395cfb208fcd3bf681b1a089
Pascale-Nasr <pascale.a.nasr@gmail.com>
2020-09-26 11:34:25 +0300

    uploading all piAutomation Folder

---
+++ data/proCommerceData.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+ 
+
+    test : {
+
+ 
+
+        name :"Karim",
+
+        email:"Karim@gmail.com",
+
+        pass :"Karimpass",
+
+        datebirth: "01/02/2001"
+
+ 
+
+    }
+
+ 
+
+}
